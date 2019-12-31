13ffd09ee1af28fbdc951e0ad24b5ef4a8d17f47
WhiteDark <sasha.2000ibr@gmail.com>
2019-12-31 13:58:47 +0300

    base

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('about/', views.index),
+    path('projects/', views.index),
+    path('contact/', views.index),
+
     #path('id<int:video_id>/', views.detail, name = 'detail'),
 
 ]
