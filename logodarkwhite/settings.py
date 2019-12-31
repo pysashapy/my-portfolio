eb99adbc1004a468fccc040c7856aa933d31b4d3
WhiteDark <sasha.2000ibr@gmail.com>
2019-12-31 15:56:44 +0300

    Add test2

--- logodarkwhite/settings.py
+++ logodarkwhite/settings.py
@@ -110,4 +110,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
